85acf32ec8a414ccecabafc55ac1862ebfd75a19
Lukas Wais <lukas.wais@outlook.de>
2020-04-28 20:04:43 +0200

    Implemented a WebView, now you can browse 9gag :)

--- source/smart.mirror/src/main/java/lukas/wais/smart/mirror/controller/MainController.java
+++ source/smart.mirror/src/main/java/lukas/wais/smart/mirror/controller/MainController.java
@@ -1,5 +1,13 @@
 package lukas.wais.smart.mirror.controller;
-
+import javafx.fxml.FXML;
+import javafx.scene.web.*;
 public class MainController {
-
+	
+	@FXML
+	private WebView testView = new WebView();
+	
+	@FXML
+	private void initialize() {
+		testView.getEngine().load("https://www.9gag.com");
+	}
 }
