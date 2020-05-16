44b2981ecff7cc2cea1d1d04bf3491704ed48faa
Lukas Wais <lukas.wais@outlook.de>
2020-05-16 21:27:39 +0200

    Updated Person
    added some default methods, toString etc.
    
    and added a List to store the widgets

--- source/smart.mirror/src/main/java/lukas/wais/smart/mirror/model/Person.java
+++ source/smart.mirror/src/main/java/lukas/wais/smart/mirror/model/Person.java
@@ -1,5 +1,7 @@
 package lukas.wais.smart.mirror.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javafx.beans.property.StringProperty;
@@ -9,6 +11,7 @@ public class Person {
 	private final StringProperty firstName;
 	private final StringProperty secondName;
 	private final StringProperty email;
+	private final List<Integer> widgets;
 
 	public Person(StringProperty firstName, StringProperty secondName, StringProperty email) {
 		super();
@@ -16,6 +19,7 @@ public class Person {
 		this.firstName = firstName;
 		this.secondName = secondName;
 		this.email = email;
+		this.widgets = new ArrayList<>();
 	}
 
 	public String getID() {
@@ -33,4 +37,40 @@ public class Person {
 	public StringProperty getEmail() {
 		return email;
 	}
+
+	public List<Integer> getWidgets() {
+		return widgets;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [ID=" + ID + ", firstName=" + firstName + ", secondName=" + secondName + ", email=" + email
+				+ ", widgets=" + widgets + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		return true;
+	}
+
 }
