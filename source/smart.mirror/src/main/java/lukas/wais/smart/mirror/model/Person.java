db6dbc3116fb7af2b2c8c35e1d55d1fb426a540a
Lukas Wais <lukas.wais@outlook.de>
2020-05-29 12:42:08 +0200

    Changed Person

--- source/smart.mirror/src/main/java/lukas/wais/smart/mirror/model/Person.java
+++ source/smart.mirror/src/main/java/lukas/wais/smart/mirror/model/Person.java
@@ -2,40 +2,74 @@ package lukas.wais.smart.mirror.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import javafx.beans.property.StringProperty;
 
 public class Person {
-	private final String ID;
+	private int ID;
 	private final StringProperty firstName;
 	private final StringProperty secondName;
+	private final StringProperty nickname;
 	private final StringProperty email;
 	private final List<Integer> widgets;
 
-	public Person(StringProperty firstName, StringProperty secondName, StringProperty email) {
+	public Person(StringProperty firstName, StringProperty secondName, StringProperty nickname, StringProperty email) {
 		super();
-		this.ID = UUID.randomUUID().toString();
 		this.firstName = firstName;
 		this.secondName = secondName;
+		this.nickname = nickname;
 		this.email = email;
 		this.widgets = new ArrayList<>();
 	}
+	
+	public Person(StringProperty firstName, StringProperty secondName, StringProperty nickname, StringProperty email, int ID) {
+		super();
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.nickname = nickname;
+		this.email = email;
+		this.widgets = new ArrayList<>();
+		this.ID = ID;
+	}
 
-	public String getID() {
+	public int getID() {
 		return ID;
 	}
 
-	public StringProperty getFirstName() {
+	public StringProperty firstNameProperty() {
 		return firstName;
 	}
 
-	public StringProperty getSecondName() {
+	public StringProperty secondNameProperty() {
 		return secondName;
 	}
 
-	public StringProperty getEmail() {
+	public StringProperty nicknameProperty() {
+		return nickname;
+	}
+
+	public StringProperty emailProperty() {
 		return email;
+	}
+
+	public String getFirstName() {
+		return firstName.get();
+	}
+
+	public String getSecondName() {
+		return secondName.get();
+	}
+
+	public String getNickname() {
+		return nickname.get();
+	}
+
+	public String getEmail() {
+		return email.get();
+	}
+
+	public void setID(int iD) {
+		ID = iD;
 	}
 
 	public List<Integer> getWidgets() {
@@ -52,7 +86,7 @@ public class Person {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		result = prime * result + ID;
 		return result;
 	}
 
@@ -65,10 +99,7 @@ public class Person {
 		if (getClass() != obj.getClass())
 			return false;
 		Person other = (Person) obj;
-		if (ID == null) {
-			if (other.ID != null)
-				return false;
-		} else if (!ID.equals(other.ID))
+		if (ID != other.ID)
 			return false;
 		return true;
 	}
