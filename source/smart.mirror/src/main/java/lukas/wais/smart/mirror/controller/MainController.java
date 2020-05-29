cdb7e166af7e9ece85b283b9e4bc3037e45bbb32
omarduenas <omarduenas@192.168.8.117>
2020-05-29 18:57:04 +0200

    DB XML

--- source/smart.mirror/src/main/java/lukas/wais/smart/mirror/controller/MainController.java
+++ source/smart.mirror/src/main/java/lukas/wais/smart/mirror/controller/MainController.java
@@ -1,6 +1,13 @@
 package lukas.wais.smart.mirror.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.Document;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,24 +20,58 @@ public class MainController {
 	/*
 	 * nice MainController should come here
 	 */
-    @FXML
-    private Pane webView;
+	@FXML
+	private Pane webView;
 
-    @FXML
-    void settingsBtn() {
-    	System.out.println(getClass().getResource("../fxml/CreateUserUI.fxml"));
-    	
-    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/CreateUserUI.fxml"));
-        try {
-            Parent parent = fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setTitle("Create New User");
-            stage.setScene(new Scene(parent));
-            stage.show();
-        } catch (IOException e) {
-            System.out.println("Could create user ui");
-            e.printStackTrace();
-        }
-    }
+	@FXML
+	void initialize() {
+		try {
+			dbToXML();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void dbToXML() throws TransformerException, ParserConfigurationException, SQLException {
+		Document doc = null;
+		try {
+			doc = new TableToXML().generateXML();
+		} catch (TransformerException | ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// XML to table data
+		try {
+			new TableToXML().xmlToTable(doc);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	void settingsBtn() {
+		System.out.println(getClass().getResource("../fxml/CreateUserUI.fxml"));
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/CreateUserUI.fxml"));
+		try {
+			Parent parent = fxmlLoader.load();
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Create New User");
+			stage.setScene(new Scene(parent));
+			stage.show();
+		} catch (IOException e) {
+			System.out.println("Could create user ui");
+			e.printStackTrace();
+		}
+	}
 }
